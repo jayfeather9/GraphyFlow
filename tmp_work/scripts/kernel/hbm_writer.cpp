b3f4aeb88c9837e354c02b41bd916087171dcdec
Feiyang Wu <jayfeather9@qq.com>
2025-10-28 17:57:32 +0800

    update to 1 big 0 little

--- tmp_work/scripts/kernel/hbm_writer.cpp
+++ tmp_work/scripts/kernel/hbm_writer.cpp
@@ -145,67 +145,72 @@ write_out:
 }
 
 extern "C" void
-hbm_writer(bus_word_t *node_props_1, bus_word_t *node_props_2,
-           bus_word_t *node_props_3, bus_word_t *node_props_4,
-           bus_word_t *node_props_5, bus_word_t *output_1, bus_word_t *output_2,
-           bus_word_t *output_3, bus_word_t *output_4, bus_word_t *output_5,
-           uint32_t dst_num_1, uint32_t dst_num_2, uint32_t dst_num_3,
-           uint32_t dst_num_4, uint32_t dst_num_5,
-           hls::stream<ppb_request_pkt_t> &ppb_req_stream_1,
-           hls::stream<ppb_request_pkt_t> &ppb_req_stream_2,
-           hls::stream<ppb_request_pkt_t> &ppb_req_stream_3,
-           hls::stream<cacheline_request_pkt_t> &cacheline_req_stream_4,
-           hls::stream<cacheline_request_pkt_t> &cacheline_req_stream_5,
-           hls::stream<ppb_response_pkt_t> &ppb_resp_stream_1,
-           hls::stream<ppb_response_pkt_t> &ppb_resp_stream_2,
-           hls::stream<ppb_response_pkt_t> &ppb_resp_stream_3,
-           hls::stream<cacheline_response_pkt_t> &cacheline_resp_stream_4,
-           hls::stream<cacheline_response_pkt_t> &cacheline_resp_stream_5,
+hbm_writer(bus_word_t *node_props_1, 
+        //    bus_word_t *node_props_2,
+        //    bus_word_t *node_props_3, bus_word_t *node_props_4,
+        //    bus_word_t *node_props_5, 
+           bus_word_t *output_1, 
+        //    bus_word_t *output_2,
+        //    bus_word_t *output_3, bus_word_t *output_4, bus_word_t *output_5,
+           uint32_t dst_num_1, 
+        //    uint32_t dst_num_2, uint32_t dst_num_3,
+        //    uint32_t dst_num_4, uint32_t dst_num_5,
+        //    hls::stream<ppb_request_pkt_t> &ppb_req_stream_1,
+        //    hls::stream<ppb_request_pkt_t> &ppb_req_stream_2,
+        //    hls::stream<ppb_request_pkt_t> &ppb_req_stream_3,
+           hls::stream<cacheline_request_pkt_t> &cacheline_req_stream_1,
+        //    hls::stream<cacheline_request_pkt_t> &cacheline_req_stream_5,
+        //    hls::stream<ppb_response_pkt_t> &ppb_resp_stream_1,
+        //    hls::stream<ppb_response_pkt_t> &ppb_resp_stream_2,
+        //    hls::stream<ppb_response_pkt_t> &ppb_resp_stream_3,
+           hls::stream<cacheline_response_pkt_t> &cacheline_resp_stream_1,
+        //    hls::stream<cacheline_response_pkt_t> &cacheline_resp_stream_5,
            hls::stream<cacheline_data_pkt_t> &cacheline_data_stream_1,
-           hls::stream<cacheline_data_pkt_t> &cacheline_data_stream_2,
-           hls::stream<cacheline_data_pkt_t> &cacheline_data_stream_3,
-           hls::stream<cacheline_data_pkt_t> &cacheline_data_stream_4,
-           hls::stream<cacheline_data_pkt_t> &cacheline_data_stream_5,
-           hls::stream<write_burst_pkt_t> &write_burst_stream_1,
-           hls::stream<write_burst_pkt_t> &write_burst_stream_2,
-           hls::stream<write_burst_pkt_t> &write_burst_stream_3,
-           hls::stream<write_burst_pkt_t> &write_burst_stream_4,
-           hls::stream<write_burst_pkt_t> &write_burst_stream_5) {
+        //    hls::stream<cacheline_data_pkt_t> &cacheline_data_stream_2,
+        //    hls::stream<cacheline_data_pkt_t> &cacheline_data_stream_3,
+        //    hls::stream<cacheline_data_pkt_t> &cacheline_data_stream_4,
+        //    hls::stream<cacheline_data_pkt_t> &cacheline_data_stream_5,
+           hls::stream<write_burst_pkt_t> &write_burst_stream_1
+        //    hls::stream<write_burst_pkt_t> &write_burst_stream_2,
+        //    hls::stream<write_burst_pkt_t> &write_burst_stream_3,
+        //    hls::stream<write_burst_pkt_t> &write_burst_stream_4,
+        //    hls::stream<write_burst_pkt_t> &write_burst_stream_5
+) {
     // --- Interface Pragmas ---
     // These pragmas map the pointers to separate AXI memory interfaces (gmem1,
     // gmem2, gmem3), allowing for parallel access to different HBM banks.
 #pragma HLS INTERFACE m_axi port = node_props_1 offset = slave bundle = gmem1
 #pragma HLS INTERFACE m_axi port = output_1 offset = slave bundle = gmem1
 
-#pragma HLS INTERFACE m_axi port = node_props_2 offset = slave bundle = gmem2
-#pragma HLS INTERFACE m_axi port = output_2 offset = slave bundle = gmem2
+// #pragma HLS INTERFACE m_axi port = node_props_2 offset = slave bundle = gmem2
+// #pragma HLS INTERFACE m_axi port = output_2 offset = slave bundle = gmem2
 
-#pragma HLS INTERFACE m_axi port = node_props_3 offset = slave bundle = gmem3
-#pragma HLS INTERFACE m_axi port = output_3 offset = slave bundle = gmem3
+// #pragma HLS INTERFACE m_axi port = node_props_3 offset = slave bundle = gmem3
+// #pragma HLS INTERFACE m_axi port = output_3 offset = slave bundle = gmem3
 
-#pragma HLS INTERFACE m_axi port = node_props_4 offset = slave bundle = gmem4
-#pragma HLS INTERFACE m_axi port = output_4 offset = slave bundle = gmem4
+// #pragma HLS INTERFACE m_axi port = node_props_4 offset = slave bundle = gmem4
+// #pragma HLS INTERFACE m_axi port = output_4 offset = slave bundle = gmem4
 
-#pragma HLS INTERFACE m_axi port = node_props_5 offset = slave bundle = gmem5
-#pragma HLS INTERFACE m_axi port = output_5 offset = slave bundle = gmem5
+// #pragma HLS INTERFACE m_axi port = node_props_5 offset = slave bundle = gmem5
+// #pragma HLS INTERFACE m_axi port = output_5 offset = slave bundle = gmem5
 
     // All scalar arguments and pointer addresses are mapped to a single control
     // bus.
 #pragma HLS INTERFACE s_axilite port = node_props_1 bundle = control
-#pragma HLS INTERFACE s_axilite port = node_props_2 bundle = control
-#pragma HLS INTERFACE s_axilite port = node_props_3 bundle = control
-#pragma HLS INTERFACE s_axilite port = node_props_4 bundle = control
-#pragma HLS INTERFACE s_axilite port = node_props_5 bundle = control
+// #pragma HLS INTERFACE s_axilite port = node_props_2 bundle = control
+// #pragma HLS INTERFACE s_axilite port = node_props_3 bundle = control
+// #pragma HLS INTERFACE s_axilite port = node_props_4 bundle = control
+// #pragma HLS INTERFACE s_axilite port = node_props_5 bundle = control
 #pragma HLS INTERFACE s_axilite port = output_1 bundle = control
-#pragma HLS INTERFACE s_axilite port = output_2 bundle = control
-#pragma HLS INTERFACE s_axilite port = output_3 bundle = control
-#pragma HLS INTERFACE s_axilite port = output_4 bundle = control
-#pragma HLS INTERFACE s_axilite port = output_5 bundle = control
+// #pragma HLS INTERFACE s_axilite port = output_2 bundle = control
+// #pragma HLS INTERFACE s_axilite port = output_3 bundle = control
+// #pragma HLS INTERFACE s_axilite port = output_4 bundle = control
+// #pragma HLS INTERFACE s_axilite port = output_5 bundle = control
 #pragma HLS INTERFACE s_axilite port = dst_num_1 bundle = control
-#pragma HLS INTERFACE s_axilite port = dst_num_2 bundle = control
-#pragma HLS INTERFACE s_axilite port = dst_num_3 bundle = control
-#pragma HLS INTERFACE s_axilite port = dst_num_4 bundle = control
-#pragma HLS INTERFACE s_axilite port = dst_num_5 bundle = control
+// #pragma HLS INTERFACE s_axilite port = dst_num_2 bundle = control
+// #pragma HLS INTERFACE s_axilite port = dst_num_3 bundle = control
+// #pragma HLS INTERFACE s_axilite port = dst_num_4 bundle = control
+// #pragma HLS INTERFACE s_axilite port = dst_num_5 bundle = control
 #pragma HLS INTERFACE s_axilite port = return bundle = control
 
     // --- Dataflow Pragma ---
@@ -218,20 +223,20 @@ hbm_writer(bus_word_t *node_props_1, bus_word_t *node_props_2,
     // The first argument (0, 1, 2) is a constant integer used by HLS to create
     // three distinct hardware instances of each function.
 
-    little_node_prop_loader(0, node_props_1, dst_num_1, ppb_req_stream_1,
-                            ppb_resp_stream_1, cacheline_data_stream_1);
-    little_node_prop_loader(1, node_props_2, dst_num_2, ppb_req_stream_2,
-                            ppb_resp_stream_2, cacheline_data_stream_2);
-    little_node_prop_loader(2, node_props_3, dst_num_3, ppb_req_stream_3,
-                            ppb_resp_stream_3, cacheline_data_stream_3);
-    big_node_prop_loader(3, node_props_4, dst_num_4, cacheline_req_stream_4,
-                         cacheline_resp_stream_4, cacheline_data_stream_4);
-    big_node_prop_loader(4, node_props_5, dst_num_5, cacheline_req_stream_5,
-                         cacheline_resp_stream_5, cacheline_data_stream_5);
+    // little_node_prop_loader(0, node_props_1, dst_num_1, ppb_req_stream_1,
+    //                         ppb_resp_stream_1, cacheline_data_stream_1);
+    // little_node_prop_loader(1, node_props_2, dst_num_2, ppb_req_stream_2,
+    //                         ppb_resp_stream_2, cacheline_data_stream_2);
+    // little_node_prop_loader(2, node_props_3, dst_num_3, ppb_req_stream_3,
+    //                         ppb_resp_stream_3, cacheline_data_stream_3);
+    big_node_prop_loader(3, node_props_1, dst_num_1, cacheline_req_stream_1,
+                         cacheline_resp_stream_1, cacheline_data_stream_1);
+    // big_node_prop_loader(4, node_props_5, dst_num_5, cacheline_req_stream_5,
+    //                      cacheline_resp_stream_5, cacheline_data_stream_5);
 
     write_out(0, output_1, dst_num_1, write_burst_stream_1);
-    write_out(1, output_2, dst_num_2, write_burst_stream_2);
-    write_out(2, output_3, dst_num_3, write_burst_stream_3);
-    write_out(3, output_4, dst_num_4, write_burst_stream_4);
-    write_out(4, output_5, dst_num_5, write_burst_stream_5);
+    // write_out(1, output_2, dst_num_2, write_burst_stream_2);
+    // write_out(2, output_3, dst_num_3, write_burst_stream_3);
+    // write_out(3, output_4, dst_num_4, write_burst_stream_4);
+    // write_out(4, output_5, dst_num_5, write_burst_stream_5);
 }
