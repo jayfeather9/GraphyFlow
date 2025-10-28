46f20a46580921a3c764055680400bafca4b02d6
Feiyang Wu <jayfeather9@qq.com>
2025-10-28 19:11:47 +0800

    modify to 2b2l

--- tmp_work/scripts/host/host_config.h
+++ tmp_work/scripts/host/host_config.h
@@ -4,13 +4,13 @@
 #include <stdint.h>
 
 #define BIG_KERNEL_NUM 2
-#define LITTLE_KERNEL_NUM 3
+#define LITTLE_KERNEL_NUM 2
 
 #define NUM_KERNEL (BIG_KERNEL_NUM + LITTLE_KERNEL_NUM)
 
-#define LITTLE_KERNEL_HBM_EDGE_ID {0, 2, 4}
-#define LITTLE_KERNEL_HBM_NODE_ID {1, 3, 5}
-#define BIG_KERNEL_HBM_EDGE_ID {6, 8}
-#define BIG_KERNEL_HBM_NODE_ID {7, 9}
+#define LITTLE_KERNEL_HBM_EDGE_ID {0, 1}
+#define LITTLE_KERNEL_HBM_NODE_ID {9, 10}
+#define BIG_KERNEL_HBM_EDGE_ID {28, 30}
+#define BIG_KERNEL_HBM_NODE_ID {11, 12}
 
 #endif /* __HOST_CONFIG_H__ */
